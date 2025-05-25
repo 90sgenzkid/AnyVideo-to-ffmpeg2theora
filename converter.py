0e0e8fe9f7c776d15ee77302625199950b34f697
Luna <lunathegamer99@gmail.com>
2025-05-25 15:17:08 +0300

    Adding comments
    For future coding and forks by users

--- converter.py
+++ converter.py
@@ -1,28 +1,29 @@
-import os, shutil
+import os
 from glob import glob
 # Any Video to ffmpeg2theora for Stalker Series, Making Cutscenes
 # By TheSparrowhawk
 # 2025
 
-theFolder = "videos"
-current_ext = {".avi"}
+theFolder = "videos" #the folder for videos
+current_ext = {".avi"} #to get only .avi video to convert
 
+#The folder where you put videos on
 getFiles = [os.path.abspath(f) for f in glob(os.path.join(theFolder, "*"))]
 
 for file_path in getFiles:
         
-        filename = os.path.splitext(file_path)[0]
-        
-        ext = os.path.splitext(file_path)[1].lower()
-        
-        print(filename)
-        print(ext)
+        filename = os.path.splitext(file_path)[0] #Gets Name
+        ext = os.path.splitext(file_path)[1].lower() #Gets File Extension
     
+        #Makes converted file new name
         output_path = os.path.join(theFolder, filename)
         print(f"Converting the file from {file_path} to {output_path}")
 
+        #Start converting
         os.system(f"ffmpeg2theora-0.16.exe {os.path.abspath(file_path)}")
-            
+
+#Removes .avi files after converting them to .ogg            
 if ext.endswith(".avi"): 
     os.remove(file_path)
+
 print("JOB IS DONE")
